22cbf5c47461107fe5039acb4ad422fc1a80963e
Stephen Werbeck <SWerbeck@yahoo.com>
2023-08-25 20:23:02 -0400

    worked on displaying time

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,24 @@
-import { React, useState, useRef } from "react";
+import { React, useState, useRef, useEffect } from "react";
 
 function Timer() {
   const [seconds, setSeconds] = useState(0);
+  const [displaySeconds, setDisplaySeconds] = useState(0);
+  const [displayMinutes, setDisplayMinutes] = useState(0);
+  const [displayHours, setDisplayHours] = useState(0);
 
   const timerId = useRef();
+
+  function timeDisplay(totalSeconds) {
+    const totalMinutes = Math.floor(totalSeconds / 60);
+
+    const secs = totalSeconds % 60;
+    const hours = Math.floor(totalMinutes / 60);
+    const mins = totalMinutes % 60;
+
+    setDisplaySeconds(secs);
+    setDisplayMinutes(mins);
+    setDisplayHours(hours);
+  }
 
   const startTimer = () => {
     timerId.current = setInterval(() => {
@@ -20,9 +35,17 @@ function Timer() {
     setSeconds(0);
   };
 
+  useEffect(() => {
+    timeDisplay(seconds);
+  }, seconds);
+
   return (
     <div>
       <p>secs: {seconds}</p>
+
+      <p>
+        time: {displayHours}:{displayMinutes}:{displaySeconds}
+      </p>
       <button onClick={startTimer}> Start </button>
       <button onClick={stopTimer}>Stop</button>
       <button onClick={resetTimer}>Reset</button>
