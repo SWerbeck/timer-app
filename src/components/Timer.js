c1270274d9b3b8f459415e5dc3db4e3a03cbfac5
Stephen Werbeck <SWerbeck@yahoo.com>
2023-08-27 13:45:12 -0400

    made start stop work as same button

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,6 +5,7 @@ function Timer() {
   const [displaySeconds, setDisplaySeconds] = useState(0);
   const [displayMinutes, setDisplayMinutes] = useState(0);
   const [displayHours, setDisplayHours] = useState(5);
+  const [start, setStart] = useState(false);
 
   const timerId = useRef();
 
@@ -24,11 +25,13 @@ function Timer() {
     timerId.current = setInterval(() => {
       setSeconds((prev) => (prev += 1));
     }, 1000);
+    setStart(true);
   };
 
   const stopTimer = () => {
     clearInterval(timerId.current);
     timerId.current = 0;
+    setStart(false);
   };
 
   const resetTimer = () => {
@@ -45,10 +48,14 @@ function Timer() {
 
       <p>
         time: {displayHours}:{displayMinutes}:{displaySeconds}
-     
       </p>
-      <button onClick={startTimer}> Start </button>
-      <button onClick={stopTimer}>Stop</button>
+
+      {start ? (
+        <button onClick={stopTimer}>Stop</button>
+      ) : (
+        <button onClick={startTimer}> Start </button>
+      )}
+
       <button onClick={resetTimer}>Reset</button>
     </div>
   );
