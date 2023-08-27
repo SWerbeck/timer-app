82796a5970123807b30b5e08efd42e2d37b16111
Stephen Werbeck <SWerbeck@yahoo.com>
2023-08-27 18:32:51 -0400

    added hr min sec inputs

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,16 +1,26 @@
 import { React, useState, useRef, useEffect } from "react";
 
 function Timer() {
+  /* counter everything runs off of */
   const [seconds, setSeconds] = useState(0);
+  /* hours, minutes and seconds displayed on the page counting up or down */
   const [displaySeconds, setDisplaySeconds] = useState(0);
   const [displayMinutes, setDisplayMinutes] = useState(0);
-  const [displayHours, setDisplayHours] = useState(5);
+  const [displayHours, setDisplayHours] = useState(0);
+  /* input times for countdown*/
+  const [inputHours, setInputHours] = useState(0);
+  const [inputMinutes, setInputMinutes] = useState(0);
+  const [inputSeconds, setInputSeconds] = useState(0);
+  /* for making button switch start/stop */
   const [start, setStart] = useState(false);
-  const [countUp, setCountUp] = useState(true);
-  const [startTime, setStartTime] = useState(3);
+  /* for making counter count up or down */
+  const [countUp, setCountUp] = useState(false);
+  /* start time when counting down */
+  const [startTime, setStartTime] = useState(0);
 
   const timerId = useRef();
 
+  /*converts secs to hr:min:sec for count up */
   function timeDisplay(totalSeconds) {
     const totalMinutes = Math.floor(totalSeconds / 60);
 
@@ -23,6 +33,7 @@ function Timer() {
     setDisplayHours(hours);
   }
 
+  /*converts secs to hr:min:sec for count down */
   function timeDisplayCountdown(totalSeconds) {
     if (totalSeconds === 0) {
       stopTimer();
@@ -38,11 +49,12 @@ function Timer() {
     setDisplayMinutes(mins);
     setDisplayHours(hours);
 
-    if (displayHours === 0 && displayMinutes == 0 && displaySeconds == 0) {
+    if (displayHours === 0 && displayMinutes === 0 && displaySeconds === 0) {
       stopTimer();
     }
   }
 
+  /* starts the counter */
   const startTimer = () => {
     timerId.current = setInterval(() => {
       setSeconds((prev) => (prev += 1));
@@ -50,16 +62,39 @@ function Timer() {
     setStart(true);
   };
 
+  /* stops the counter */
   const stopTimer = () => {
     clearInterval(timerId.current);
     timerId.current = 0;
     setStart(false);
   };
 
+  /* rests the counter */
   const resetTimer = () => {
     setSeconds(0);
   };
 
+  /* sets count up or down */
+  const countUpDown = (event) => {
+    setCountUp(event.target.value);
+  };
+
+  /* sets start time for count down */
+  const handleStartHours = (event) => {
+    setInputHours(event.target.value);
+  };
+  const handleStartMinutes = (event) => {
+    setInputMinutes(event.target.value);
+  };
+  const handleStartSeconds = (event) => {
+    setInputSeconds(event.target.value);
+  };
+
+  const timeToSeconds = () => {
+    setStartTime(inputHours * 3600 + inputMinutes * 60 + inputSeconds);
+  };
+
+  /* watches seconds state and runs timeDisplay function everytime it changes */
   useEffect(() => {
     if (countUp) {
       timeDisplay(seconds);
@@ -68,12 +103,49 @@ function Timer() {
     }
   }, [seconds]);
 
+  useEffect(() => {
+    timeToSeconds();
+  }, [inputHours, inputMinutes, inputSeconds]);
+
   return (
     <div>
-      <p>secs: {seconds}</p>
+      <p>(this will be removed for final version) secs: {seconds}</p>
+
+      {!start && !countUp ? (
+        <form>
+          <label>Hr:</label>
+          <input
+            type={"number"}
+            value={inputHours}
+            onChange={handleStartHours}
+          />{" "}
+          <label>Min:</label>
+          <input
+            type={"number"}
+            value={inputMinutes}
+            onChange={handleStartMinutes}
+          />{" "}
+          <label>Sec:</label>
+          <input
+            type={"number"}
+            value={inputSeconds}
+            onChange={handleStartSeconds}
+          />{" "}
+        </form>
+      ) : (
+        ""
+      )}
+
+      <label>Stopwatch / Count Down: </label>
+      <select value={false} onChange={countUpDown}>
+        <option value={true}>Stopwatch</option>
+        <option value={false}>Count Down</option>
+      </select>
 
       <p>
-        time: {displayHours}:{displayMinutes}:{displaySeconds}
+        time: {displayHours < 10 ? "0" + displayHours : displayHours}:
+        {displayMinutes < 10 ? "0" + displayMinutes : displayMinutes}:
+        {displaySeconds < 10 ? "0" + displaySeconds : displaySeconds}
       </p>
 
       {start ? (
