9f281e1416b47b82807107fdc71adc154c7a2c37
Lena Al <lena.al.developer@gmail.com>
2023-09-02 00:04:26 -0400

    added some general first draft styling

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,6 @@
 import { React, useState, useRef, useEffect } from "react";
 import ReactSwitch from "react-switch";
+import ".././App.css";
 
 function Timer() {
   /* counter everything runs off of */
@@ -23,13 +24,11 @@ function Timer() {
   const [alertSecs, setAlertSecs] = useState(0);
   const [alertMins, setAlertMins] = useState(0);
   const [alertHr, setAlertHr] = useState(0);
- 
 
   const timerId = useRef();
 
   // .........................................................................
   // .........................................................................
- 
 
   /*converts secs to hr:min:sec for count up */
   function timeDisplay(totalSeconds) {
@@ -46,11 +45,9 @@ function Timer() {
 
   /*converts secs to hr:min:sec for count down */
   function timeDisplayCountdown(totalSeconds) {
-
     if (totalSeconds <= 0) {
       stopTimer();
-    setSeconds(0)
-      
+      setSeconds(0);
     }
 
     const totalMinutes = Math.floor(totalSeconds / 60);
@@ -68,28 +65,24 @@ function Timer() {
     }
   }
 
-
-
   /* starts the counter */
   const startTimer = () => {
-    let counter = 0
-    console.log(startTime)
+    let counter = 0;
+    console.log(startTime);
     timerId.current = setInterval(() => {
       setSeconds((prev) => (prev += 1));
-      if (altertInterval){
-        if (counter === altertInterval-1){
-          console.log("alert!!!!")
+      if (altertInterval) {
+        if (counter === altertInterval - 1) {
+          console.log("alert!!!!");
         }
-        if (counter === altertInterval){
-          
-          counter = 0
+        if (counter === altertInterval) {
+          counter = 0;
         }
-        counter++
-        console.log(counter)
+        counter++;
+        console.log(counter);
       }
-     
     }, 1000);
- 
+
     setStart(true);
   };
 
@@ -107,58 +100,52 @@ function Timer() {
 
   /* sets count up or down */
   const countUpDown = (val) => {
-   setCountUp(val)
-   setSeconds(0)
+    setCountUp(val);
+    setSeconds(0);
   };
 
   /* sets start time for count down */
   const handleStartHours = (event) => {
-    setInputHours(event.target.value * 1) ;
-    resetTimer()
+    setInputHours(event.target.value * 1);
+    resetTimer();
   };
   const handleStartMinutes = (event) => {
     setInputMinutes(event.target.value * 1);
-    resetTimer()
+    resetTimer();
   };
   const handleStartSeconds = (event) => {
     setInputSeconds(event.target.value * 1);
-    resetTimer()
+    resetTimer();
   };
 
   const timeToSeconds = () => {
-    setStartTime((inputHours * 3600) + (inputMinutes * 60) + (inputSeconds * 1));
-
+    setStartTime(inputHours * 3600 + inputMinutes * 60 + inputSeconds * 1);
   };
 
-    /* sets alert interval time */
-    const handleAlertHr = (event) => {
-      setAlertHr(event.target.value * 1);
-    };
-    const handleAlertMin = (event) => {
-      setAlertMins(event.target.value * 1);
-    };
-    const handleAlertSec = (event) => {
-      setAlertSecs(event.target.value * 1);
-    };
+  /* sets alert interval time */
+  const handleAlertHr = (event) => {
+    setAlertHr(event.target.value * 1);
+  };
+  const handleAlertMin = (event) => {
+    setAlertMins(event.target.value * 1);
+  };
+  const handleAlertSec = (event) => {
+    setAlertSecs(event.target.value * 1);
+  };
 
-    const alertTimeToSeconds = () => {
-      setAlertInterval((alertHr * 3600) + (alertMins * 60) + (alertSecs *1));
-  
-    };
-     
-    const countDownAlert = () => {
-      if (startTime - seconds === 0){
-        console.log("TIME'S UP!!!!")
-        resetTimer()
-      }
+  const alertTimeToSeconds = () => {
+    setAlertInterval(alertHr * 3600 + alertMins * 60 + alertSecs * 1);
+  };
+
+  const countDownAlert = () => {
+    if (startTime - seconds === 0) {
+      console.log("TIME'S UP!!!!");
+      resetTimer();
     }
+  };
 
-
-
-// .........................................................................
-// .........................................................................
-
-
+  // .........................................................................
+  // .........................................................................
 
   /* watches seconds state and runs timeDisplay function everytime it changes */
   useEffect(() => {
@@ -166,9 +153,8 @@ function Timer() {
       timeDisplay(seconds);
     } else {
       timeDisplayCountdown(startTime - seconds);
-      countDownAlert()
+      countDownAlert();
     }
-
   }, [seconds, startTime, countUp]);
 
   useEffect(() => {
@@ -179,48 +165,46 @@ function Timer() {
     alertTimeToSeconds();
   }, [alertHr, alertMins, alertSecs]);
 
-
   // .........................................................................
   // .........................................................................
-
-
 
   return (
-    <div>
+    <div className="timer-container">
       <p>(this will be removed for final version) secs: {seconds}</p>
 
-   <h4>Stopwatch / Countdown</h4>
-   <ReactSwitch checked={countUp} onChange={countUpDown}/>
+      <div className="d-flex justify-content-center">
+        <h4>Stopwatch / Countdown</h4>
+        <ReactSwitch checked={countUp} onChange={countUpDown} />
+      </div>
 
       {!start && !countUp ? (
-        <form>
+        <form className="d-flex justify-content-center">
           <label>Hr:</label>
           <input
             type={"number"}
             value={inputHours}
             onChange={handleStartHours}
-          />{" "}
+          />
           <label>Min:</label>
           <input
             type={"number"}
             value={inputMinutes}
             onChange={handleStartMinutes}
-          />{" "}
+          />
           <label>Sec:</label>
           <input
             type={"number"}
             value={inputSeconds}
             onChange={handleStartSeconds}
-          />{" "}
+          />
         </form>
       ) : (
         ""
       )}
 
-
- {!start ? (
-        <form>
-          <label>alert every:  </label>
+      {!start ? (
+        <form className="d-flex justify-content-center">
+          <label>alert every: </label>
           <label>Hr:</label>
           <input
             type={"number"}
@@ -241,7 +225,11 @@ function Timer() {
           />{" "}
         </form>
       ) : (
-       <p> alert every:  hr: {alertHr} : mins: {alertMins} : secs: {alertSecs}  </p>    )}
+        <p>
+          {" "}
+          alert every: hr: {alertHr} : mins: {alertMins} : secs: {alertSecs}
+        </p>
+      )}
 
       {/* <label>Stopwatch / Count Down: </label>
       <select value={countUp} onChange={countUpDown}>
@@ -249,22 +237,40 @@ function Timer() {
         <option value={"countDown"}>Count Down</option>
       </select> */}
 
-      <p>
+      <p className="d-flex justify-content-center p-5">
         time: {displayHours < 10 ? "0" + displayHours : displayHours}:
         {displayMinutes < 10 ? "0" + displayMinutes : displayMinutes}:
         {displaySeconds < 10 ? "0" + displaySeconds : displaySeconds}
       </p>
 
-      {start ? (
-        <button onClick={stopTimer}>Stop</button>
-      ) : (
-        <button onClick={startTimer}> Start </button>
-      )}
-
-      <button onClick={resetTimer}>Reset</button>
+      <div className="buttons d-flex justify-content-center p-5">
+        {start ? (
+          <button
+            onClick={stopTimer}
+            type="button"
+            className="btn btn-danger me-3 btn-lg"
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            onClick={startTimer}
+            type="button"
+            className="btn btn-primary me-3 btn-lg"
+          >
+            Start
+          </button>
+        )}
+        <button
+          onClick={resetTimer}
+          type="button"
+          className="btn btn-warning me-3 btn-lg"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Timer;
-
